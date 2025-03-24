d191861192ef595c4b57abff5f66519278199d94
Habiba Elhadi <habibaelhadi9@gmail.com>
2025-03-24 23:35:30 +0200

    Add hourly data to home screen

--- Clima/app/src/main/java/com/example/clima/utilites/ForecastItem.kt
+++ Clima/app/src/main/java/com/example/clima/utilites/ForecastItem.kt
@@ -5,20 +5,29 @@ import com.example.clima.composableFunctions.home.view.detectIcon
 import com.example.clima.model.ForeCast
 
 data class ForecastItem(
-    @DrawableRes var icon : Int,
-    var dayOfWeek : String,
-    var date : String,
-    var temp : String,
-    var airQuality : String,
-    var airQualityIndicatorColor : String = "#f9cf5f",
-    var isSelected : Boolean = false
+    @DrawableRes var icon: Int,
+    var dayOfWeek: String,
+    var date: String,
+    var temp: String,
+    var airQuality: String,
+    var airQualityIndicatorColor: String = "#f9cf5f",
+    var isSelected: Boolean = false,
+    var desc: String = "Desc",
+    var hour : String = "12:00"
 )
 
-fun ForeCast.ForecastWeather.convertAPIResponse(forecast : ForeCast.ForecastWeather,isSelected: Boolean): ForecastItem{
-    return ForecastItem( icon = detectIcon(forecast.weather[0].icon) ,
-    dayOfWeek = displayDate(forecast.dt.toLong(),"EEE"),
-    date = displayDate(forecast.dt.toLong(),"dd MMM"),
-    temp = forecast.main.temp.toInt().toString(),
-    airQuality = forecast.main.feels_like.toString(),
-    isSelected = isSelected)
+fun ForeCast.ForecastWeather.convertAPIResponse(
+    forecast: ForeCast.ForecastWeather,
+    isSelected: Boolean = false
+): ForecastItem {
+    return ForecastItem(
+        icon = detectIcon(forecast.weather[0].icon),
+        dayOfWeek = displayDate(forecast.dt.toLong(), "EEE"),
+        date = displayDate(forecast.dt.toLong(), "dd MMM"),
+        temp = forecast.main.temp.toInt().toString(),
+        airQuality = forecast.main.feels_like.toString(),
+        isSelected = isSelected,
+        desc = forecast.weather[0].description,
+        hour = forecast.dt_txt.split(" ")[1]
+    )
 }
