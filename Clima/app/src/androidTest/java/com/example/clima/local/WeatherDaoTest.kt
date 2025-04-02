f2485ad80414cf7a5840c07a1f03fef9977204a7
Habiba Elhadi <habibaelhadi9@gmail.com>
2025-04-03 01:43:56 +0200

    Handle dao testing

--- Clima/app/src/androidTest/java/com/example/clima/local/WeatherDaoTest.kt
+++ Clima/app/src/androidTest/java/com/example/clima/local/WeatherDaoTest.kt
@@ -1,22 +1,16 @@
 package com.example.clima.local
 
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.example.clima.model.Alarm
-import com.example.clima.model.FavouritePOJO
-import com.example.clima.model.HomePOJO
-import io.mockk.Runs
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
-import io.mockk.verify
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,63 +19,60 @@ import org.junit.runner.RunWith
 @SmallTest
 class WeatherDaoTest {
 
+    private lateinit var database : AppDataBase
     private lateinit var weatherDao: WeatherDao
 
     @Before
     fun setUp() {
-        weatherDao = mockk(relaxed = true)
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            AppDataBase::class.java
+        ).build()
+        weatherDao = database.weatherDao()
+    }
+
+    @After
+    fun closeDatabase(){
+        database.close()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getAlarms_returnAlarms() = runTest {
+    fun insertAlarmsThenDeleteOne_returnsOneAlarm() = runTest {
 
         // Given
-        val expectedAlarms = listOf(mockk<Alarm>())
-
-        every { weatherDao.getAlarms() } returns flowOf(expectedAlarms)
+        weatherDao.insertAlarm(Alarm(1,"01:00","01:01"))
+        weatherDao.insertAlarm(Alarm(3,"01:02","01:04"))
+        weatherDao.deleteAlarm(Alarm(3,"01:02","01:04"))
 
         // When
-        val result = weatherDao.getAlarms()
+        val result = weatherDao.getAlarms().first()
         advanceUntilIdle()
 
         // Then
-        result.collect { alarms ->
-            assertEquals(expectedAlarms, alarms)
-        }
+        assertEquals(1, result.size)
+        assertEquals("01:00", result.first().startTime)
+        assertEquals("01:01", result.first().endTime)
 
-        verify { weatherDao.getAlarms() }
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun deleteWeather_callWeatherDaoDeleteWeather() = runTest {
-        // Given
-        val weather = mockk<FavouritePOJO>()
-
-        coEvery { weatherDao.deleteWeather(weather) } just Runs
-
-        // When
-        weatherDao.deleteWeather(weather)
+    fun insertAlarm_ShouldInsertAlarmCorectly() = runTest {
+        // Given a test alarm
+        val alarm = Alarm(
+            1,
+            "08:00",
+            "08:01"
+        )
+        //when
+        weatherDao.insertAlarm(alarm)
         advanceUntilIdle()
 
-        // Then
-        coVerify { weatherDao.deleteWeather(weather) }
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun insertCacheHome_callWeatherDaoInsertCacheHome() = runTest {
-        // Given
-        val homeData = mockk<HomePOJO>()
-
-        coEvery { weatherDao.insertCacheHome(homeData) } just Runs
-
-        // When
-        weatherDao.insertCacheHome(homeData)
-        advanceUntilIdle()
-
-        // Then
-        coVerify { weatherDao.insertCacheHome(homeData) }
+        //then
+        val alarms = weatherDao.getAlarms().first()
+        assertEquals(1, alarms.size)
+        assertEquals("08:00", alarms.first().startTime)
+        assertEquals("08:01", alarms.first().endTime)
     }
 }
